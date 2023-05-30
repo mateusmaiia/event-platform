1284bf54d3bd68e1045f8b6dafc50fc339531174
Maia-Mateus <matteusmaia889@gmail.com>
2023-05-30 18:19:53 -0300

    Update Lesson.tsx

--- src/cmoponents/Lesson.tsx
+++ src/cmoponents/Lesson.tsx
@@ -1,4 +1,4 @@
-import { CheckCircle } from 'phosphor-react'
+import { CheckCircle, Lock } from 'phosphor-react'
 
 interface LessonProps {
     title: string;
@@ -18,10 +18,17 @@ export function Lesson(props: LessonProps) {
 
             <div className="rounded border border-gray-500 p-4 mt-2">
                 <header className="flex items-center justify-between">
-                    <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
-                        <CheckCircle size={20} />
-                        Conteúdo liberado
-                    </span>
+                    {isLessonAvailable ? (
+                        <span className="flex items-center gap-2 text-sm text-blue-500 font-medium">
+                            <CheckCircle size={20} />
+                            Conteúdo liberado
+                        </span>
+                    ) : (
+                        <span className="flex items-center gap-2 text-sm text-orange-500 font-medium">
+                            <Lock size={20} />
+                            Em breve
+                        </span>
+                    )}
                     <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
                         {props.type == 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
                     </span>
