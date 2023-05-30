fe21fba73ae1e12aaee9bcb699cee98e643a1a7d
Maia-Mateus <matteusmaia889@gmail.com>
2023-05-30 17:13:33 -0300

    customizing Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import { Sidebar } from './cmoponents/Sidebar'
 import { Video } from './cmoponents/Video'
 
 export function App() {
-  return (<>
-    <Header />
-    <main className='flex '>
-      <Video />
-      <Sidebar />
-    </main>
-  </>
+  return (
+    <div className='flex flex-col min-h-screen'>
+      <Header />
+      <main className='flex flex-1'>
+        <Video />
+        <Sidebar />
+      </main>
+    </div>
   )
 }
 
