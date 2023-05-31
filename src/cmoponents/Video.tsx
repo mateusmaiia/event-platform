b396cccc9d74b8b007b20060ccf7d7f85fc1d7b5
Maia-Mateus <matteusmaia889@gmail.com>
2023-05-31 15:04:40 -0300

    hovering the buttons

--- src/cmoponents/Video.tsx
+++ src/cmoponents/Video.tsx
@@ -22,7 +22,7 @@ export function Video() {
                             <DiscordLogo size={24}/>
                             COMUNIDADE DO DISCORD
                         </a>
-                        <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2  justify-center hover:bg-blue-500 hover:text-gray-900">
+                        <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2  justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
                             <Lightning size={24}/>
                             Acesse o desafio
                         </a>
