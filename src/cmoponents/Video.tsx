452acc95dde6fe5487919eb50bb2aa48fde84419
Maia-Mateus <matteusmaia889@gmail.com>
2023-05-31 14:52:13 -0300

    Update Video.tsx

--- src/cmoponents/Video.tsx
+++ src/cmoponents/Video.tsx
@@ -1,3 +1,5 @@
+import { DiscordLogo, Lightning } from "phosphor-react";
+
 export function Video() {
     return (
         <div className="flex-1">
@@ -15,9 +17,20 @@ export function Video() {
                             Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
                         </p>
                     </div>
-                    <div></div>
+                    <div className="flex flex-col gap-4">
+                        <a href="#" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2  justify-center hover:bg-green-700 transition-colors">
+                            <DiscordLogo size={24}/>
+                            COMUNIDADE DO DISCORD
+                        </a>
+                        <a href="#" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2  justify-center hover:bg-blue-500 hover:text-gray-900">
+                            <Lightning size={24}/>
+                            Acesse o desafio
+                        </a>
+                    </div>
                 </div>
-                <div></div>
+                <div>
+
+                </div>
             </div>
         </div>
     )
