c519720c61c76b2be213496f88cb0e3445369b13
Maia-Mateus <matteusmaia889@gmail.com>
2023-06-03 13:39:09 -0300

    Adding libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from './cmoponents/Header'
 import { Sidebar } from './cmoponents/Sidebar'
 import { Video } from './cmoponents/Video'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
 
 export function App() {
   return (
+    <ApolloProvider client={client}>
     <div className='flex flex-col min-h-screen'>
       <Header />
       <main className='flex flex-1'>
@@ -11,6 +14,7 @@ export function App() {
         <Sidebar />
       </main>
     </div>
+    </ApolloProvider>
   )
 }
 
